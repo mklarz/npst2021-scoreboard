5c2b30fc1908af359a7def042a1e2d63ee4c6950
mklarz <mklarz@protonmail.com>
2021-12-04 20:27:41 +0100

    [IGNORE] track profiles

--- check_scoreboard.sh
+++ check_scoreboard.sh
@@ -5,6 +5,7 @@
 ## CONSTANTS
 BASE_URL="https://wiarnvnpsjysgqbwigrn.supabase.co"
 LOGIN_URL="$BASE_URL/auth/v1/token?grant_type=password"
+PROFILES_URL="$BASE_URL/rest/v1/profiles?select=*"
 SCOREBOARD_URL="$BASE_URL/rest/v1/scoreboard?select=*"
 SCRIPT_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
@@ -15,9 +16,12 @@ source "$SCRIPT_PATH/.env"
 
 # Download newest scoreboard
 curl -s "$SCOREBOARD_URL" -H "apikey: $DASS_APIKEY" -o "$SCRIPT_PATH/scoreboard.min.json"
+# Download profiles
+curl -s "$PROFILES_URL" -H "apikey: $DASS_APIKEY" -o "$SCRIPT_PATH/profiles.min.json"
 
 # Let's "prettify" the json as well
 cat "$SCRIPT_PATH/scoreboard.min.json" | jq > "$SCRIPT_PATH/scoreboard.json"
+cat "$SCRIPT_PATH/profiles.min.json" | jq > "$SCRIPT_PATH/profiles.json"
 
 if [[ `git status --porcelain` ]]; then
   echo "There are differences, updating"
