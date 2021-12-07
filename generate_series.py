6100005720c93d5f4ec372a069cd7d6e4a29dd03
mklarz <mklarz@protonmail.com>
2021-12-07 23:37:55 +0100

    [IGNORE] add currently maxed useds

--- generate_series.py
+++ generate_series.py
@@ -5,7 +5,8 @@ from git import Repo
 
 # Quick, dirty and very inefficent script to generate formatted data for the line graph
 LIMIT = 10
-START_TIMESTAMP = datetime(2021, 12, 1, 17, tzinfo=timezone.utc).isoformat()
+START_DATETIME = datetime(2021, 12, 1, 17, tzinfo=timezone.utc)
+START_TIMESTAMP = START_DATETIME.isoformat()
 
 def format_item(timestamp, value):
     return [timestamp, value]
@@ -16,9 +17,11 @@ commits = list(repo.iter_commits(master))[::-1]
 
 last_scoreboard = None
 users = {}
+users_currently_maxed_count_series = []
 print("Parsing {} commits and creating series.json".format(len(commits)))
 for commit in commits:
-    print("Parsing: {} - {}".format(commit.hexsha, commit.committed_date))
+    commit_datetime = datetime.utcfromtimestamp(commit.committed_date).replace(tzinfo=timezone.utc)
+    print("Parsing: {} - {}".format(commit.hexsha, commit_datetime))
     content = repo.git.show('{}:{}'.format(commit.hexsha, "scoreboard.min.json")).strip()
     try:
         scoreboard = json.loads(content)
@@ -26,12 +29,25 @@ for commit in commits:
         # print(scoreboard)
         # print("Error for scoreboard ", e)
         continue
+
+    # Which day in the calendar are we on?
+    current_advent_day = ((commit_datetime - START_DATETIME).days) + 1
+    # TODO: remove sundays
+
+    max_possible_solves = current_advent_day
+    # Remove sundays
+    max_possible_solves -= current_advent_day // 7
+
     last_scoreboard = scoreboard
+    users_currently_maxed_count = 0
     for scoreboard_user in scoreboard:
         current_score = int(scoreboard_user["score"])
         current_solves = int(scoreboard_user["num_solves"])
         user_id = scoreboard_user["user_id"]
         last_solve = scoreboard_user["latest_solve_time"]
+
+        if current_solves == max_possible_solves:
+            users_currently_maxed_count += 1
 
         if user_id not in users:
             users[user_id] = {
@@ -51,6 +67,8 @@ for commit in commits:
                 user["score"].append(format_item(last_solve, current_score))
 
         users[user_id] = user
+    print("Users with max solves:", users_currently_maxed_count)
+    users_currently_maxed_count_series.append([commit_datetime.isoformat(), users_currently_maxed_count])
 
 series = {
     "users": [],
@@ -68,7 +86,10 @@ for index, scoreboard_user in enumerate(last_scoreboard[:LIMIT]):
     } 
     series["users"].append({**base, **{"data": user["score"]}});
 
-with open("series.json", "w") as f:
-    json.dump(series, f)
+with open("series.json", "w") as fd:
+    json.dump(series, fd)
+
+with open("currently_maxed_users.json", "w") as fd:
+    json.dump(users_currently_maxed_count_series, fd)
 
 print("Done!")
